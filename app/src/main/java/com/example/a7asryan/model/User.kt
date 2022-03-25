1a72bdd446d4d5baa1092b4a9babbefbe694e85f
MohabElziny <mohabziny@gmail.com>
2022-03-25 17:07:11 +0200

    create dao with dummy data

--- app/src/main/java/com/example/a7asryan/model/User.kt
+++ app/src/main/java/com/example/a7asryan/model/User.kt
@@ -1,11 +1,14 @@
 package com.example.a7asryan.model
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-//dummy class
 @Entity(tableName = "user")
 data class User(
     private val userName: String,
+    @PrimaryKey
+    @NonNull
     private val email: String,
     private val password: String,
     private val phoneNumber: String
