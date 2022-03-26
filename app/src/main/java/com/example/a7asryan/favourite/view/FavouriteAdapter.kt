b802dbaba4bd39479ee9d0019fba6c3da8b2a2d7
Ahmed Mohamed Abdallah <benten12536@gmail.com>
2022-03-26 16:15:24 +0200

    favourite ui

--- app/src/main/java/com/example/a7asryan/favourite/view/FavouriteAdapter.kt
+++ app/src/main/java/com/example/a7asryan/favourite/view/FavouriteAdapter.kt
@@ -1,15 +1,22 @@
 package com.example.a7asryan.favourite.view
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a7asryan.databinding.CardFavouriteBinding
 
 class FavouriteAdapter : RecyclerView.Adapter<FavouriteAdapter.ViewHolder>() {
-    class ViewHolder {
+    class ViewHolder(
+        private val binding: CardFavouriteBinding
+    ) :RecyclerView.ViewHolder(binding.root){
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteAdapter.ViewHolder {
+        return ViewHolder(
+            CardFavouriteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
